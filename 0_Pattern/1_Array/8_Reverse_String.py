35ed4a5d95047e820dc4e2b8ddfb3d51182e0c29
andreyhitoshi1997 <andreyhitoshi1997@gmamil.com>
2026-03-23 15:59:40 -0300

    feat: Add function to reverse a string using a two-pointer approach.

--- 0_Pattern/1_Array/8_Reverse_String.py
+++ 0_Pattern/1_Array/8_Reverse_String.py
@@ -1,7 +1,8 @@
 def reverString(s):
+    arr = [ch for ch in s]
     l, r = 0, len(s) -1
-    while l < r :
-        s[l], s[r] = s[r], s[l]
+    while l < r:
+        arr[l], arr[r] = arr[r], arr[l]
         l += 1
         r -= 1
-    return s
+    return "".join(arr)
