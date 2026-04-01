3e306e36e7d54a9d9ef1b0fbed39efef8db69760
andreyhitoshi1997 <andreyhitoshi1997@gmamil.com>
2026-04-01 19:39:49 -0300

    Subindo

---
+++ 0_Pattern/2_linked_List/1_linked_list.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.value, end=" <-> ")
+            current = current.next
+        print("None")
+        
---
+++ 0_Pattern/2_linked_List/2_inverter_linked_list.py
@@ -0,0 +1,10 @@
+from typing import Optional
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        new_list = None
+        while head:
+            next_node = head.next
+            head.next = new_list
+            new_list = head
+            head = next_node
+        return new_list
